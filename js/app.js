a936d97c5c9cb68145e5e480db4f937dd49d012d
Kuba Ślawski <kubaslawski@gmail.com>
2020-11-04 15:29:59 +0100

    Initializing app

---
+++ js/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+
+  return (
+    <>
+      <h1>To Do App</h1>
+    </>
+  )
+}
+
+
+
+ReactDOM.render(<App/>, document.querySelector("#app"));
